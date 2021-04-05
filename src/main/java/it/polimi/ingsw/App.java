85dad47964a272b3b1b1652f6c25a22fe346df9a
valer <valeriapante@gmail.com>
2021-04-05 10:42:29 +0200

    Adds useful methods

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,14 +1,13 @@
 
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.Abilities.Ability;
-import it.polimi.ingsw.Cards.ActionToken;
-import it.polimi.ingsw.Cards.DevCard;
-import it.polimi.ingsw.Cards.DevCardType;
-import it.polimi.ingsw.Cards.LeaderCard;
-import it.polimi.ingsw.Enums.Colour;
-import it.polimi.ingsw.Decks.*;
-import it.polimi.ingsw.Player.DevSlot;
+
+import it.polimi.ingsw.Enums.Resource;
+
+
+import java.util.Scanner;
+
+import static it.polimi.ingsw.Enums.Resource.*;
 
 /**
  * Hello world!
