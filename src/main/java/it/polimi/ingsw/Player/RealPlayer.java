d8007e5f3e72c53753da9284994effcd0ceee84e
Daniel Pozza <daniel.pozza@mail.polimi.it>
2021-04-07 14:52:36 +0200

    Code cleaning + getAllProductionPowers

--- src/main/java/it/polimi/ingsw/Player/RealPlayer.java
+++ src/main/java/it/polimi/ingsw/Player/RealPlayer.java
@@ -1,20 +1,17 @@
 package it.polimi.ingsw.Player;
 
 import it.polimi.ingsw.Abilities.ProductionPower.ProductionPower;
-import it.polimi.ingsw.Cards.DevCard;
 import it.polimi.ingsw.Cards.LeaderCard;
 import it.polimi.ingsw.Cards.PopeFavorCard;
 import it.polimi.ingsw.Deposit.Depot;
 import it.polimi.ingsw.Deposit.Shelf;
-import it.polimi.ingsw.Deposit.Payable;
-import it.polimi.ingsw.Enums.LeaderCardType;
 import it.polimi.ingsw.Enums.Resource;
 import it.polimi.ingsw.Exceptions.WeDontDoSuchThingsHere;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EnumMap;
-import java.util.HashMap;
+import java.util.LinkedList;
 
 public class RealPlayer extends Player{
 
@@ -77,10 +74,6 @@ public class RealPlayer extends Player{
         this.actionDone = actionDone;
     }
 
-    public ProductionPower getBasicProductionPower(){
-        return this.basicProductionPower;
-    }
-
     //--- Selection Section ---
     //public void paySelected(){
     //    for (Payable toPay : this.selected.keySet()){
@@ -96,6 +89,9 @@ public class RealPlayer extends Player{
     //------
 
     //---Getters---
+    public ProductionPower getBasicProductionPower(){
+        return this.basicProductionPower;
+    }
     public boolean isActionDone(){
         return this.actionDone;
     }
@@ -111,34 +107,74 @@ public class RealPlayer extends Player{
     public LeaderCard[] getLeaderCards(){
         return this.leaderCards.toArray(new LeaderCard[0]);
     }
+    public LinkedList<ProductionPower> getAllProductionPowers(){
+        return this.calculateAllProductionPowers();
+    }
+    public EnumMap<Resource, Integer> getResourcesOwned(){
+        return this.resourcesOwned();
+    }
     //-----
 
     // returns null if the player owns no Resources, otherwise it will return an EnumMap with the copy of all resources
-    public EnumMap<Resource, Integer> resourcesOwned(){
+    private EnumMap<Resource, Integer> resourcesOwned(){
         Depot allResources = new Depot();
 
         if ( !this.depot.isEmpty())
             allResources.addEnumMap(this.depot.content());
 
-        for (Shelf shelf: shelves)
-            if ( !shelf.isEmpty())
+        for (Shelf shelf: this.shelves)
+            if (!shelf.isEmpty())
                 allResources.addEnumMap(shelf.content());
 
-        for (LeaderCard lc : leaderCards)
-            if (lc.getType() == LeaderCardType.STORAGE){
+        for (LeaderCard leaderCard : leaderCards){
+            if (leaderCard.hasBeenPlayed()){
                 try{
-                    if ( !lc.getAbility().isEmpty())
-                        allResources.addEnumMap(lc.getAbility().getContent());
+                    if (!leaderCard.getAbility().isEmpty())
+                        allResources.addEnumMap(leaderCard.getAbility().getContent());
                 }
-                catch (WeDontDoSuchThingsHere e) {
-                    e.printStackTrace();
+                catch (WeDontDoSuchThingsHere e){
+                    //pass
                 }
             }
+        }
         return (allResources.isEmpty()) ? null : allResources.content();
     }
 
+
     //it can be used to know if the player owns any Resource
-    public boolean isBroken(){
+    public boolean isBroken() {
         return (this.resourcesOwned() == null);
     }
+
+    //Returns all the production powers that the player has
+    private LinkedList<ProductionPower> calculateAllProductionPowers(){
+        LinkedList<ProductionPower> allProductionPowers = new LinkedList<>();
+
+        //default
+        allProductionPowers.add(this.basicProductionPower);
+
+        //ProdPowers in devslots
+        for (DevSlot devSlot : this.devSlots){
+            if (!devSlot.isEmpty()){
+                //here i should have a dev card
+                allProductionPowers.add(devSlot.topCard().getProdPower());
+            }
+        }
+
+        //leaderCards
+        for (LeaderCard leaderCard : this.leaderCards){
+            if (leaderCard.hasBeenPlayed()) {
+                try {
+                    //no Exceptions -> the leaderCard type = ProdPower
+                    allProductionPowers.add(leaderCard.getAbility().getProductionPower());
+                } catch (WeDontDoSuchThingsHere e) {
+                    //pass
+                }
+            }
+        }
+        return allProductionPowers;
+    }
+
+
+
 }
