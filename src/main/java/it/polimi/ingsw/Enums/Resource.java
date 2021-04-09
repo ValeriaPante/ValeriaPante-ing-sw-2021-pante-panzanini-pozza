d34a796792810fda0d8dfc6c721940d49a0cd532
AlbertoPanzanini <10629004@polimi.it>
2021-04-09 15:38:00 +0200

    testing the Test

--- src/main/java/it/polimi/ingsw/Enums/Resource.java
+++ src/main/java/it/polimi/ingsw/Enums/Resource.java
@@ -7,14 +7,14 @@ import java.util.Map;
 public enum Resource {
     COIN("CO"), STONE("ST"), SERVANT("SE"), SHIELD("SH"), FAITH, WHITE, ANY;
 
-    private /*static*/ final Map<String, Resource> resourcesAlias = new HashMap<>();
+    private static final Map<String, Resource> resourcesAlias = new HashMap<>();
 
     private Resource(String... aliases) {
         for(final String alias : aliases)
             resourcesAlias.put(alias, this);
     }
 
-    public /*static*/ Resource fromAlias(String alias) {
+    public static Resource fromAlias(String alias) {
         return resourcesAlias.get(alias);
     }
 
---
+++ src/test/java/org/example/DepotCalssTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import it.polimi.ingsw.Deposit.Depot;
+import it.polimi.ingsw.Enums.Resource;
+
+public class DepotCalssTest {
+    Depot tryDepot = new Depot(){{singleAdd(Resource.COIN);}}
+
+}
