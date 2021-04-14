542623721832a1c30720fdc512930b41c394a172
Daniel Pozza <daniel.pozza@mail.polimi.it>
2021-04-14 16:42:25 +0200

    Tests

---
+++ src/test/java/AbilitesTest/AbilityProductionTest.java
@@ -0,0 +1,76 @@
+package AbilitesTest;
+
+import it.polimi.ingsw.Abilities.Ability;
+import it.polimi.ingsw.Enums.LeaderCardType;
+import it.polimi.ingsw.Enums.Resource;
+import it.polimi.ingsw.Exceptions.WeDontDoSuchThingsHere;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.EnumMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AbilityProductionTest {
+    private Ability ability;
+    private final EnumMap<Resource, Integer> input = new EnumMap<>(Resource.class){{
+        put(Resource.COIN, 3);
+        put(Resource.STONE, 1);
+    }};
+
+    @BeforeEach
+    public void init(){
+        this.ability = new Ability(this.input, LeaderCardType.PRODPOWER);
+    }
+
+    @Test
+    public void discountsMethods(){
+        assertThrows(WeDontDoSuchThingsHere.class, () -> this.ability.getDiscount());
+    }
+
+    @Test
+    public void productionPowerMethods(){
+        //verifico che non crei eccezione
+        assertAll(()->this.ability.getProductionPower());
+
+        //verifico che l'input nell'abilità sia uguale al parametro inserito
+        assertEquals(this.input, this.ability.getProductionPower().getInput());
+
+        //verifico che l'output sia quello che mi aspetto (1FP, 1ANY)
+        EnumMap<Resource, Integer> outputExpected = new EnumMap<>(Resource.class){{
+            put(Resource.ANY, 1);
+            put(Resource.FAITH, 1);
+        }};
+        assertEquals(outputExpected, this.ability.getProductionPower().getOutput());
+
+
+        //verifico che modificando l'input non cambia l'oggetto
+        this.input.remove(Resource.STONE);
+        assertTrue(this.ability.getProductionPower().getInput().containsKey(Resource.STONE));
+
+        //MANCA LA TOSTRING
+    }
+
+    @Test
+    public void storageAbilityMethod(){
+        assertThrows(WeDontDoSuchThingsHere.class, () -> this.ability.add(Resource.ANY));
+        assertThrows(WeDontDoSuchThingsHere.class, () -> this.ability.remove(Resource.ANY));
+        assertThrows(WeDontDoSuchThingsHere.class, () -> this.ability.isEmpty());
+        assertThrows(WeDontDoSuchThingsHere.class, () -> this.ability.getContent());
+        assertThrows(WeDontDoSuchThingsHere.class, () -> this.ability.getCapacity());
+        assertThrows(WeDontDoSuchThingsHere.class, () -> this.ability.contains(new EnumMap<>(Resource.class)));
+        assertThrows(WeDontDoSuchThingsHere.class, () -> this.ability.pay(new EnumMap<>(Resource.class)));
+        assertThrows(WeDontDoSuchThingsHere.class, () -> this.ability.select(Resource.ANY));
+        assertThrows(WeDontDoSuchThingsHere.class, () -> this.ability.deselect(Resource.ANY));
+        assertThrows(WeDontDoSuchThingsHere.class, () -> this.ability.getSelected());
+    }
+
+    @Test
+    public void transmutationMethods(){
+        assertThrows(WeDontDoSuchThingsHere.class, () -> this.ability.getWhiteInto());
+    }
+
+
+
+}
---
+++ src/test/java/AbilitesTest/ProductionPowerTest.java
@@ -0,0 +1,88 @@
+package AbilitesTest;
+
+import it.polimi.ingsw.Abilities.ProductionPower.ProductionPower;
+import it.polimi.ingsw.Enums.Resource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.EnumMap;
+
+public class ProductionPowerTest {
+
+    private EnumMap<Resource, Integer> input;
+    private EnumMap<Resource, Integer> output;
+    private ProductionPower productionPower;
+
+    @BeforeEach
+    private void init(){
+        this.input = new EnumMap<>(Resource.class){{
+            put(Resource.STONE, 4);
+            put(Resource.SHIELD, 5);
+        }};
+        this.output = new EnumMap<>(Resource.class){{
+           put(Resource.COIN, 1);
+           put(Resource.SERVANT, 2);
+        }};
+        this.productionPower = new ProductionPower(this.input, this.output);
+    }
+
+    @Test
+    public void getInputOutputTest(){
+        //verifico che inserisca correttamente l'input
+        EnumMap<Resource, Integer> input = this.productionPower.getInput();
+        for (Resource resource : Resource.values()){
+            if (this.input.containsKey(resource)){
+                assertTrue(this.input.containsKey(resource) && input.containsKey(resource));
+                assertEquals(this.input.get(resource), input.get(resource));
+            }
+            else{
+                assertTrue(!this.input.containsKey(resource) && !input.containsKey(resource));
+            }
+        }
+
+        //verifico che inserisca correttamente l'output
+        EnumMap<Resource, Integer> output = this.productionPower.getOutput();
+        for (Resource resource : Resource.values()){
+            if (this.output.containsKey(resource)){
+                assertTrue(this.output.containsKey(resource) && output.containsKey(resource));
+                assertEquals(this.output.get(resource), output.get(resource));
+            }
+            else{
+                assertTrue(!this.output.containsKey(resource) && !output.containsKey(resource));
+            }
+        }
+    }
+
+    @Test
+    public void toStringTest(){
+        assertEquals("( STONE:4  SHIELD:5 ) -> ( COIN:1  SERVANT:2 )", this.productionPower.toString());
+    }
+
+    @Test
+    public void equalsTest(){
+        //creo un nuovo productionPower uguale e verifico che l'equals funzioni
+        EnumMap<Resource, Integer> input = new EnumMap<>(Resource.class){{
+            put(Resource.STONE, 4);
+            put(Resource.SHIELD, 5);
+        }};
+        EnumMap<Resource, Integer> output = new EnumMap<>(Resource.class){{
+            put(Resource.COIN, 1);
+            put(Resource.SERVANT, 2);
+        }};
+
+        assertTrue(this.productionPower.equals(new ProductionPower(input, output)));
+    }
+
+
+    @Test
+    public void stabilityTest(){
+        //modificando i parametri di input e output il ProductionPower non cambia
+        this.input.remove(Resource.STONE);
+        assertTrue(this.productionPower.getInput().containsKey(Resource.STONE));
+        this.output.remove(Resource.COIN);
+        assertTrue(this.productionPower.getOutput().containsKey(Resource.COIN));
+    }
+
+}
