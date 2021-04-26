05dfd413a79fd5afba9c1e81dddde2b78fedb3d3
Daniel Pozza <daniel.pozza@mail.polimi.it>
2021-04-27 01:04:54 +0200

    added method isAffordableSomehow

--- src/main/java/it/polimi/ingsw/Controller/CardActionController.java
+++ src/main/java/it/polimi/ingsw/Controller/CardActionController.java
@@ -1,4 +1,38 @@
 package it.polimi.ingsw.Controller;
 
+import it.polimi.ingsw.Deposit.Depot;
+import it.polimi.ingsw.Enums.Resource;
+import it.polimi.ingsw.Game.Table;
+
+import java.util.EnumMap;
+
 public class CardActionController extends SelectionController{
+
+    public CardActionController(Table table){
+        super(table);
+    }
+
+    protected boolean isAffordableSomehow(EnumMap<Resource, Integer> checkMap) {
+        Depot allResources = new Depot() {{
+            addEnumMap(table.turnOf().getResourcesOwned());
+        }};
+        EnumMap<Resource, Integer> copy = checkMap.clone();
+
+        if (!checkMap.containsKey(Resource.ANY)) {
+            return allResources.contains(checkMap);
+        }
+
+        //qui in poi ho le any
+        int anyAmount = copy.get(Resource.ANY);
+        copy.remove(Resource.ANY);
+        int otherResourcesAmount = 0;
+        for (int value : copy.values()) {
+            otherResourcesAmount += value;
+        }
+        return (allResources.contains(copy) && allResources.countAll() >= otherResourcesAmount + anyAmount);
+    }
+
+
+
+
 }
