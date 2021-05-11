9b5c2a60e343ee8bb56a1979382a1d6debbfe70b
AlbertoPanzanini <10629004@polimi.it>
2021-05-11 09:59:53 +0200

    ControllerSwitch not common methods

--- src/main/java/it/polimi/ingsw/Controller/ControllerSwitch.java
+++ src/main/java/it/polimi/ingsw/Controller/ControllerSwitch.java
@@ -3,47 +3,93 @@ package it.polimi.ingsw.Controller;
 import it.polimi.ingsw.Messages.*;
 
 public class ControllerSwitch {
-    public void actionOnMessage(AllProductionPowerSelectionMessage message){ }
+    private BuyDevCardController buyDevCardController;
+    private GameController gameController;
+    private LeaderController leaderController;
+    private MarketController marketController;
+    private ProductionController productionController;
 
-    public void actionOnMessage(AnySelectionMessage message){ }
+    public void actionOnMessage(AllProductionPowerSelectionMessage message){
+        productionController.selectAllProductionPowers();
+    }
 
-    public void actionOnMessage(BackFromAnySelectionMessage message){ }
+    public void actionOnMessage(AnySelectionMessage message){
+        productionController.anySelection(message.getResource());
+    }
 
-    public void actionOnMessage(BuyDevCardMessage message){ }
+    public void actionOnMessage(BackFromAnySelectionMessage message){
+        productionController.backFromAnySelection();
+    }
 
-    public void actionOnMessage(CardProductionSelectionMessage message){ }
+    public void actionOnMessage(BuyDevCardMessage message){
+        buyDevCardController.buyDevCard();
+    }
 
-    public void actionOnMessage(ChooseDevCardMessage message){ }
+    public void actionOnMessage(CardProductionSelectionMessage message){
+        productionController.selectCardProduction(message.getInteger());
+    }
 
-    public void actionOnMessage(ChooseDevSlotMessage message){ }
+    public void actionOnMessage(ChooseDevCardMessage message){
+        buyDevCardController.chooseDevCard(message.getInteger());
+    }
 
-    public void actionOnMessage(DiscountAbilityMessage message){ }
+    public void actionOnMessage(ChooseDevSlotMessage message){
+        buyDevCardController.chooseDevSlot(message.getInteger());
+    }
 
-    public void actionOnMessage(EndTurnMessage message){ }
+    public void actionOnMessage(DiscountAbilityMessage message){
+        buyDevCardController.applyDiscountAbility(message.getInteger());
+    }
 
-    public void actionOnMessage(ExchangeMessage message){ }
+    public void actionOnMessage(EndTurnMessage message){
+        gameController.endTurn();
+    }
 
-    public void actionOnMessage(LeaderCardActionMessage message){ }
+    public void actionOnMessage(ExchangeMessage message){
+        marketController.exchange();
+    }
 
-    public void actionOnMessage(LeaderDiscardMessage message){ }
+    public void actionOnMessage(LeaderCardActionMessage message){
+        leaderController.actionOnLeaderCard(message.getInteger(), message.isaBoolean());
+    }
 
-    public void actionOnMessage(LeaderStorageSelectionMessage message){ }
+    public void actionOnMessage(LeaderDiscardMessage message){
+        gameController.discardLeaderCard(message.getInteger());
+    }
 
-    public void actionOnMessage(MarketSelectionMessage message){ }
+    public void actionOnMessage(LeaderStorageSelectionMessage message){}
 
-    public void actionOnMessage(MoveToLeaderStorageMessage message){ }
+    public void actionOnMessage(MarketSelectionMessage message){
+        marketController.selectFromMarket(message.getInteger(), message.isaBoolean());
+    }
 
-    public void actionOnMessage(MoveToShelfMessage message){ }
+    public void actionOnMessage(MoveToLeaderStorageMessage message){
+        marketController.moveToLeaderStorage(message.getInteger());
+    }
 
-    public void actionOnMessage(MoveToSupportContainerMessage message){ }
+    public void actionOnMessage(MoveToShelfMessage message){
+        marketController.moveToShelf(message.getInteger());
+    }
 
-    public void actionOnMessage(PaySelectedMessage message){ }
+    public void actionOnMessage(MoveToSupportContainerMessage message){
+        marketController.moveSelectedToSupportContainer();
+    }
 
-    public void actionOnMessage(ProductionActivationMessage message){ }
+    public void actionOnMessage(PaySelectedMessage message){
+        buyDevCardController.paySelected();
+    }
 
-    public void actionOnMessage(QuitFromMarketMessage message){ }
+    public void actionOnMessage(ProductionActivationMessage message){
+        productionController.activateProduction();
+    }
 
-    public void actionOnMessage(SelectResourceMessage message){ }
+    public void actionOnMessage(QuitFromMarketMessage message){
+        marketController.quit();
+    }
+
+    public void actionOnMessage(SelectResourceMessage message){
+        gameController.selectResource(message.getInteger(), message.getResource());
+    }
     
     public void actionOnMessage(ShelfDeselectionMessage message){ }
 
@@ -53,11 +99,19 @@ public class ControllerSwitch {
 
     public void actionOnMessage(StrongBoxSelectionMessage message){ }
 
-    public void actionOnMessage(SupportContainerDeselectionMessage message){ }
+    public void actionOnMessage(SupportContainerDeselectionMessage message){
+        marketController.deselectFromSupportContainer(message.getResource(), message.getInteger());
+    }
 
-    public void actionOnMessage(SupportContainerSelectionMessage message){ }
+    public void actionOnMessage(SupportContainerSelectionMessage message){
+        marketController.selectFromSupportContainer(message.getResource(), message.getInteger());
+    }
 
-    public void actionOnMessage(TakeFromMarketMessage message){ }
+    public void actionOnMessage(TakeFromMarketMessage message){
+        marketController.takeFromMarket();
+    }
 
-    public void actionOnMessage(TransmutationMessage message){ }
+    public void actionOnMessage(TransmutationMessage message){
+        marketController.selectTransmutation(message.getSerial1(), message.getQuantity1(), message.getSerial2(), message.getQuantity2());
+    }
 }
