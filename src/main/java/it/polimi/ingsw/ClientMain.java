bc4dc74550650d30ec9203e74cc081a9eefcac27
valer <valeriapante@gmail.com>
2021-06-02 11:31:08 +0200

    Client Main update

--- src/main/java/it/polimi/ingsw/ClientMain.java
+++ src/main/java/it/polimi/ingsw/ClientMain.java
@@ -8,30 +8,10 @@ public class ClientMain
 {
     public static void main( String[] args )
     {
-        Application.launch(GUI.class);
-        //gui.addObserver(client);
+        if(args.length == 0) Application.launch(GUI.class);
+        else {
 
-        /*for(int i = 0; i < 100; i++){
-            gui.updateLobbyState(i, new String[]{"valeria"+i, "alberto"+i, "daniel"+i});
         }
-        int[][] devDecks = {{1,4,2,3},{17,20,18,19},{33,36,34,35}};
-        gui.getModel().initialiseDevDecks(devDecks);
 
-        new Thread(() -> {
-            try{
-                *//*Thread.sleep(5000);
-                gui.updateLobbyState(1000, new String[]{"e", "f"});
-                gui.updateLobbyState(1, new String[]{"e", "f"});
-                gui.updateLobbyState(1001, new String[]{"e", "f"});
-                Thread.sleep(5000);
-                gui.removeLobby(1001);*//*
-                Thread.sleep(5000);
-                gui.chooseLeaderCards();
-                Thread.sleep(5000);
-                gui.startGame();
-            } catch( Exception e ){
-
-            }
-        }).start();*/
     }
 }
