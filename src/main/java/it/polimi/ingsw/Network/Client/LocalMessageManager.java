e0b08029add2b29f795c3b222b23c9fd150b2c4b
Daniel Pozza <daniel.pozza@mail.polimi.it>
2021-06-30 15:41:49 +0200

    Correct loading assets for offline game

--- src/main/java/it/polimi/ingsw/Network/Client/LocalMessageManager.java
+++ src/main/java/it/polimi/ingsw/Network/Client/LocalMessageManager.java
@@ -10,6 +10,8 @@ import it.polimi.ingsw.PreGameModel.Lobby;
 import it.polimi.ingsw.PreGameModel.User;
 import it.polimi.ingsw.View.View;
 
+import java.io.*;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 /**
@@ -19,11 +21,56 @@ public class LocalMessageManager implements MessageManager{
 
     private final InGameControllerSwitch inGameControllerSwitch;
 
+    private void copyFile(File fileToCopy, String outputDirPath){
+        if (fileToCopy.isDirectory()){
+            File dir = new File(outputDirPath + File.separator + fileToCopy.getName());
+            if (!dir.exists()){
+                dir.mkdirs();
+            }
+            for (File file : fileToCopy.listFiles()){
+                this.copyFile(file, dir.getPath());
+            }
+        }
+        else if (fileToCopy.isFile()){
+            try {
+                FileInputStream fileInputStream = new FileInputStream(fileToCopy);
+                FileOutputStream fileOutputStream = new FileOutputStream(outputDirPath + File.separator + fileToCopy.getName(), false);
+
+                try {
+                    int count;
+                    byte[] buffer = new byte[1024];
+                    while ((count = fileInputStream.read(buffer)) != -1) {
+                        fileOutputStream.write(buffer, 0, count);
+                    }
+                }catch (IOException ignored){}
+
+                fileInputStream.close();
+                fileOutputStream.close();
+            }catch (IOException ignored){}
+        }
+    }
+
+    private void settingUpAssets(){
+        try {
+            File jarFolder = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile();
+            File outputDir = new File(jarFolder.getAbsolutePath() + File.separator + "accessible");
+            if (!outputDir.exists()){
+                outputDir.mkdirs();
+            }
+            for (File file : jarFolder.listFiles()){
+                if (file.isDirectory() && !file.getPath().equals(outputDir.getPath())){
+                    this.copyFile(file, outputDir.getPath());
+                }
+            }
+        }catch (URISyntaxException ignored){}
+    }
+
     /**
      * Constructor
      * @param view view modality chosen by the player
      */
     public LocalMessageManager(View view) {
+        this.settingUpAssets();
         Lobby lobby = new Lobby(0);
         User user = new User("you", new LocalMessageSender(view));
         user.setId(1);
